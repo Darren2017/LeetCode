44405fbb2ecbf9adb79a407e91aab52e60960d3c
Darren2017 <17362990052@163.com>
2018-10-24 23:56:32 +0800

    fix: creator_id

---
+++ C++/053_MaximumSubarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int len = nums.size(), ans = 0, rans = INT_MIN;
+        for(int i = 0; i < len; i++){
+            if(ans < 0){
+                ans = 0;
+            }
+            ans += nums[i];
+            rans = max(ans, rans);
+        }
+        return rans;
+    }
+};
+
+/*
+    动态规划，前一部分如果小于0便抛弃，否则便保留。
+    还可以用分治，之后看了大佬的再加，自己还不会。
+*/
