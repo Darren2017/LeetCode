4ebc89a58f9036b52944f23a216337b2a8718a1a
Darren2017 <17362990052@163.com>
2018-11-07 16:56:41 +0800

    AC

---
+++ C++/746_MinCostClimbingStairs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int len = cost.size();
+        int finalcost[len] = {0};
+        for(int i = 2; i < len + 1; i++){
+            finalcost[i] = min(finalcost[i - 2] + cost[i - 2], finalcost[i - 1] + cost[i - 1]);
+        }
+        return finalcost[len];
+    }
+};
